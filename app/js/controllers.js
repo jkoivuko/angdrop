3be1b05b23a8df2f214959f72b50f20646be2972
Jani Koivukoski <fts@coders.fi>
2014-05-05 14:51:50 +0000

    added randomizer

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -8,7 +8,7 @@ angular.module('angdrop.controllers', [])
   }]) // location and scope needs to be as parameters 
   .controller('MyCtrl2', ["$location", "$scope", function($location, $scope) {
   $scope.username = "test";  
-  $scope.dropkey = "foobar";
+  $scope.dropkey = ""+Math.floor(Math.random()*100000000000);
 
   $scope.create = function(username, hash) {
       alert("creating "+username+hash);
